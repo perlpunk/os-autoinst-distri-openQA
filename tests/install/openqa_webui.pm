09ec7cbfa1d903e6f9a9e4e8bad20bb8927a0005
Tina Müller <tina.mueller@suse.com>
2022-07-16 00:17:42 +0200

    WIP

--- tests/install/openqa_webui.pm
+++ tests/install/openqa_webui.pm
@@ -3,80 +3,91 @@ use base "openQAcoretest";
 use testapi;
 use utils;
 
-
-sub install_from_repos {
-    diag('following https://github.com/os-autoinst/openQA/blob/master/docs/Installing.asciidoc');
-    my $add_repo;
-    die 'Needs implementation for other versions' unless get_required_var('VERSION') =~ /(tw|Tumbleweed)/;
-    my %repo_suffix = (
-        x86_64  => 'Tumbleweed',
-        aarch64 => 'Factory_ARM',
-        ppc64le => 'Factory_PowerPC'
-    );
-    my $repo = 'openSUSE_' . $repo_suffix{get_required_var('ARCH')};
-    $add_repo = "zypper -n ar -p 95 -f obs://devel:openQA/$repo openQA";
-    assert_script_run($_) foreach (split /\n/, $add_repo);
-    assert_script_run('retry -s 30 -- zypper --no-cd -n --gpg-auto-import-keys in openQA-local-db', 600);
-    my $configure = <<'EOF';
-/usr/share/openqa/script/configure-web-proxy
-sed -i -e 's/#.*method.*OpenID.*$/&\nmethod = Fake/' /etc/openqa/openqa.ini
-systemctl restart apache2
-systemctl enable --now openqa-webui
-systemctl status --no-pager openqa-webui
-systemctl enable --now openqa-scheduler
-systemctl status --no-pager openqa-scheduler
-EOF
-    assert_script_run($_) foreach (split /\n/, $configure);
-}
-
-sub install_from_git {
-    my $configure = <<'EOF';
-retry -s 30 -- zypper -n in -C 'rubygem(sass)' git-core perl-App-cpanminus perl-Module-CPANfile perl-YAML-LibYAML postgresql-server apache2
-systemctl start postgresql || systemctl status --no-pager postgresql
-su - postgres -c 'createuser root'
-su - postgres -c 'createdb -O root openqa'
-git clone https://github.com/os-autoinst/openQA.git
-cd openQA
-pkgs=$(for p in $(cpanfile-dump); do echo -n "perl($p) "; done); retry -s 30 -- zypper -n in -C $pkgs
-cpanm -nq --installdeps .
-for i in headers proxy proxy_http proxy_wstunnel rewrite ; do a2enmod $i ; done
-cp etc/apache2/vhosts.d/openqa-common.inc /etc/apache2/vhosts.d/
-sed "s/#ServerName.*$/ServerName $(hostname)/" etc/apache2/vhosts.d/openqa.conf.template > /etc/apache2/vhosts.d/openqa.conf
-systemctl restart apache2 || systemctl status --no-pager apache2
-mkdir -p /var/lib/openqa/db
-EOF
-    assert_script_run($_, 600) foreach (split /\n/, $configure);
-    script_run('env OPENQA_CONFIG=etc/openqa nohup script/openqa daemon &', 0);
-    diag('Wait until the server is responsive');
-    assert_script_run('while ! [ -f nohup.out ]; do sleep 1 ; done && grep -qP "Listening at.*(127.0.0.1|localhost)" <(tail -f -n0 nohup.out) ', 600);
-}
-
-sub install_containers {
-    assert_script_run('retry -s 30 -- zypper -n in docker git', timeout => 600);
-    assert_script_run("systemctl start docker");
-}
-
 sub run {
     send_key "ctrl-alt-f3";
     assert_screen "inst-console";
     type_string "root\n";
     assert_screen "password-prompt";
+    $testapi::password = 'nots3cr3t';
     type_string $testapi::password . "\n";
     wait_still_screen(2);
     diag('Ensure packagekit is not interfering with zypper calls');
     assert_script_run('systemctl mask --now packagekit');
     assert_script_run('zypper --no-cd -n in retry');
-    if (check_var('OPENQA_FROM_GIT', 1)) {
-        if (get_var('OPENQA_CONTAINERS')) {
-            install_containers;
-        }
-        else {
-            install_from_git;
-        }
-    }
-    else {
-        install_from_repos;
-    }
+
+    my @packages = qw( docker git );
+#    @packages = qw( chromium );
+    assert_script_run("retry -s 30 -- zypper -n in @packages", timeout => 600);
+    assert_script_run "systemctl start docker";
+    assert_script_run "docker run --rm -p 31337:31337 -d yamlio/yaml-play-sandbox:0.1.30 31337";
+    my $out = script_output "docker images";
+    diag "Docker images: >>$out<<";
+
+    sleep 1;
+    switch_to_x11;
+    sleep 1;
+    ensure_unlocked_desktop();
+    sleep 2;
+    my $url = "https://play.yaml.io/main/parser";
+    x11_start_program("firefox $url", 60, {valid => 1});
+#    x11_start_program("chromium $url", 60, {valid => 1});
+    sleep 4;
+    assert_screen 'ff-playgound-start', 10;
+
+    send_key 'ctrl-a';
+    sleep 2;
+    send_key 'delete';
+    sleep 2;
+    type_string "---\nopenQA: rocks!";
+
+    assert_screen 'ff-playgound-enter-example', 10;
+    mouse_set(232, 298);
+    mouse_click 'left';
+    sleep 2;
+    assert_screen 'ff-playgound-click-sandbox', 10;
+    mouse_set(672, 583);
+    sleep 1;
+    mouse_click 'left';
+
+    send_key 'ctrl-t';
+    type_string('https://0.0.0.0:31337/');
+    send_key 'ret';
+    assert_screen 'ff-playgound-local-cert-error', 10;
+
+    mouse_set(797, 526);
+    sleep 1;
+    mouse_click 'left';
+    assert_screen 'ff-playgound-local-cert-error-advanced', 10;
+    send_key 'end';
+    assert_screen 'ff-playgound-local-cert-error-advanced-scrolled-down', 10;
+
+    mouse_set(684, 692);
+    sleep 1;
+    mouse_click 'left';
+    assert_screen 'ff-playgound-local-cert-error-advanced-accept-risk', 10;
+
+    mouse_set(712, 494);
+    sleep 1;
+    mouse_click 'left';
+    assert_screen 'ff-playgound-local-cert-error-advanced-ok-close', 10;
+
+    sleep 1;
+    send_key 'ctrl-w';
+    assert_screen 'ff-playgound-enter-example', 10;
+
+    sleep 1;
+    send_key 'f5';
+    sleep 5;
+    assert_screen 'ff-playgound-start-with-docker', 10;
+
+
+    send_key 'ctrl-a';
+    sleep 2;
+    send_key 'delete';
+    sleep 2;
+    type_string "---\nopenQA: rocks!";
+    assert_screen 'ff-playgound-enter-example-with-docker', 10;
+
     save_screenshot;
     clear_root_console;
 }
